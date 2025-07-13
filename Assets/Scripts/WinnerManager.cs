1c8819ff573bd5844b7089ec139c4fb1e1c8ba2d
Max <maksplatonov07@mail.ru>
2025-07-13 20:30:43 +0400

    сделал меню после победы

--- Assets/Scripts/WinnerManager.cs
+++ Assets/Scripts/WinnerManager.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class WinnerManager : MonoBehaviour
 {
     public TMP_Text winText;
+    public GameObject WinnerMenuu;
+    public Button nextLevel;
+    public Button restartLevel;
+    public Button levelsMenu;
     private bool gameStop = false;
+    void Start()
+    {
+        WinnerMenuu.SetActive(false);
+        nextLevel.onClick.AddListener(LoadNextLevel);
+        restartLevel.onClick.AddListener(RestartLevel);
+        levelsMenu.onClick.AddListener(ReturnToMainMenu);
+    }
     void FixedUpdate()
     {
         if (gameStop) return;
@@ -16,15 +28,33 @@ public class WinnerManager : MonoBehaviour
         {
             string winner = chickens[0].name.Replace("(Clone)", "").Replace("_", " "); 
             ShowWinner(winner);
+            WinnerMenuu.SetActive(true);
             StopGame();
         }
         else if (chickens.Length == 0)
         {
             winText.text = "No one wins!";
+            WinnerMenuu.SetActive(true);
             StopGame();
         }
     }
+    void LoadNextLevel()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 
+    void RestartLevel()
+    {
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void ReturnToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
     void ShowWinner(string winner)
     {
         winText.text = $"{winner} wins!"; 
@@ -34,10 +64,5 @@ public class WinnerManager : MonoBehaviour
     {
         gameStop = true;
         Time.timeScale = 0f;
-    }
-    public void RestartGame()
-    {
-        Time.timeScale = 1f;
-        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
     }
 }
