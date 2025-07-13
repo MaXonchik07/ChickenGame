d46c31c942e00ccc7276d939dd8be50e046f2f1f
Gamadrila <nantix1234@mail.ru>
2025-07-13 18:46:25 +0400

    неверно выгрузил) добавил фон музыку и кнопку для неё

---
+++ Assets/MUSOR/BackMusic.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance = null;
+    public AudioSource musicSource;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject); // позволяет не удалятся объекту при переходе на другую сцену
+    }
+}
